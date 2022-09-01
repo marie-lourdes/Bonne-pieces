0be79c8236b97b7325dee48f7896e2a91e7b6d52
marie-lourdes <marielourdesleperlier64@gmail.com>
2022-09-01 19:39:46 +0200

    commi

--- avis.js
+++ avis.js
@@ -1,9 +1,1 @@
-export function ajoutListenersAvis() {
-    const piecesElements = document.querySelectorAll(".fiches article button");
 
-    for (let i = 0; i < piecesElements.length; i++) {
-      piecesElements[i].addEventListener("click", async function (event) {
-           /* ... */
-      });
-    }
-}
